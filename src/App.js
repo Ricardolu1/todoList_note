d8f8297d2beca3df1a1b80011544642b293127dd
Ricardo <568082704@qq.com>
2019-10-21 21:43:44 +0800

    尝试context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, {createContext,Component}from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SSL_OP_ALL } from 'constants';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const BaterryContext = createContext()
+const OnlineContext = createContext()
+
+function Middle() {
+  return(
+    <div>
+      <Leaf />
     </div>
-  );
+  )
 }
+function Leaf() {
+  return(
+    <BaterryContext.Consumer>
+      {battery=>(
+        <OnlineContext.Consumer>
+          {online=><h1>Battery:{battery},Online:{String(online)}</h1>}
+        </OnlineContext.Consumer>
+      )}
+    </BaterryContext.Consumer>
+  )
+}
+
+class App extends Component{
+  state = {
+    online:false,
+    battery:60  
+  }
+  render(){
+    const {battery, online} = this.state
+    return (
+      <BaterryContext.Provider value={battery}>
+          <OnlineContext.Provider value={online}>
+            <button onClick={()=>this.setState({battery:battery+1})}>press</button>
+            <button onClick={()=>this.setState({online:!online})}>switch</button>
+            <Middle />
+          </OnlineContext.Provider>
+      </BaterryContext.Provider>
+    )
+  }
+}
+
+
+
+
 
 export default App;
